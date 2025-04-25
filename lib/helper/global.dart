3f677eedfe3ae3748dc0f0dcfffd7c854f7cf2eb
VOLTAR99 <biswasgaurav28@gmail.com>
2025-04-25 17:58:54 +0530

    outputs added

--- lib/helper/global.dart
+++ lib/helper/global.dart
@@ -4,5 +4,5 @@ const appName = 'Yukta AI';
 
 late Size mq;
 
-const apiKey = 'YOUR_API_KEY';
+const apiKey = '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';
   
